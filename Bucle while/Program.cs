c6025b53898f76c98ac5b34b09f1d1f7cd253635
arielneomatrix <68985117+arielneomatrix@users.noreply.github.com>
2025-08-08 00:07:44 -0300

    Agregar archivos de proyecto.

---
+++ Bucle while/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bucle_while
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int contador = 0;
+            while (contador < 100)
+            {
+                Console.WriteLine("El contador es: " + contador);
+                contador++;
+
+                contador = contador + 2; // Incrementa el contador en 2
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
